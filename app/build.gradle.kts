bf8a5e0331fcc8fe125bc6f6c1188c93cff17dda
Dushyant <>
2024-05-10 20:00:21 +0530

    xml design of profile page (in progress)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
 dependencies {
     //circle imageview
     implementation("de.hdodenhof:circleimageview:3.1.0")
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
